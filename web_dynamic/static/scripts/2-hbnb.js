e040a128b34ba6a3c21602a99b6e8baacf9a92e4
Adejare <adejare.arasheed@gmail.com>
2024-04-07 20:07:34 +0100

    Web Dynamic

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,28 @@
+$(function () {
+  const myObjs = {};
+
+  $('input[type="checkbox"]').change(function () {
+    const dataId = $(this).attr('data-id');
+    const dataName = $(this).attr('data-name');
+
+    if ($(this).is(':checked')) {
+      myObjs[dataId] = dataName;
+    } else {
+      if (dataId in myObjs) {
+        delete myObjs[dataId];
+      }
+    }
+    $('DIV.amenities h4').text(Object.values(myObjs));
+  });
+
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (resp, status, xhr) {
+    if (xhr.status === 200) {
+      if (!($('div#api_status').hasClass('available'))) {
+        if ($('div#api_status').attr('style')) {
+          $('div#api_status').removeAttr('style');
+        }
+        $('div#api_status').addClass('available');
+      }
+    }
+  });
+});
